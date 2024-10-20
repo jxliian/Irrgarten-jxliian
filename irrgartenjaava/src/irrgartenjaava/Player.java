c616b248db6a34ae50cdeff8a0f45bcbbff87ee1
jxliian <carrionjuliann@gmail.com>
2024-10-20 18:28:27 +0200

    P2 JAVA - actualizado

--- irrgartenjaava/src/irrgartenjaava/Player.java
+++ irrgartenjaava/src/irrgartenjaava/Player.java
@@ -10,4 +10,74 @@ package irrgartenjaava;
  */
 public class Player {
     
+    private static final int MAX_WEAPONS=2;
+    private static final int MAX_SHIELDS=3;
+    private static final int INITIAL_HEALTH=10;
+    private static final int HITS2LOSE=3;
+    
+    private String name;
+    private char number;
+    private float intelligence;
+    private float strength;
+    private float health;
+    private int row;
+    private int col;
+    private int consecutiveHits=0;
+    
+    Player(char number, float intelligence, float strength){
+        throw new UnsupportedOperationException();
+    }
+    
+    public void resurrect(){
+        throw new UnsupportedOperationException();
+    }
+    
+    public int getRow(){
+        throw new UnsupportedOperationException();
+    }
+
+    public int getCol(){
+        throw new UnsupportedOperationException();
+    }
+    
+    public char getNumber(){
+        throw new UnsupportedOperationException();
+    }
+
+    public void setPos(int row, int col){
+        throw new UnsupportedOperationException();
+    }
+    
+    public boolean dead(){
+        throw new UnsupportedOperationException();
+    }
+    
+    public Directions move(Directions direction, Directions []validMoves){
+        throw new UnsupportedOperationException(); // se hace asi???
+    }                                              // directions [] me refiero
+    
+    public float attack(){
+        throw new UnsupportedOperationException();
+    }
+    
+    public boolean defend(float receivedAttack){
+        throw new UnsupportedOperationException();
+    }
+
+    public void receiveReward(){
+        throw new UnsupportedOperationException();
+    }
+    
+    @Override
+    public String toString(){
+        throw new UnsupportedOperationException();
+    }
+    
+    private void receiveWeapon(Weapon w){
+        throw new UnsupportedOperationException();
+    }
+    
+    
 }
+
+
